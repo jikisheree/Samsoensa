e39cf63e2a1af8ec752a5d63b85208088b282607
NuChyyyy <76035494+NuChyyyy@users.noreply.github.com>
2021-02-12 18:43:20 +0700

    build mainmenustate

--- Project1/Project1/Source.cpp
+++ Project1/Project1/Source.cpp
@@ -1,5 +1,7 @@
 #include <SFML/Graphics.hpp>
 #include <time.h>
+
+using namespace std;
 using namespace sf;
 
 struct point
@@ -14,13 +16,15 @@ int main()
     RenderWindow app(VideoMode(600, 853), "Raise dumb!");
     app.setFramerateLimit(60);
 
-    Texture x1, x2, x3;
+    Texture x1, x2, x3, x4, x5 , x6;
     x1.loadFromFile("images/bg1.jpg");
     x2.loadFromFile("images/platform.png");
     x3.loadFromFile("images/cat.png");
+    x4.loadFromFile("images/bgmain.jpg");
+    x5.loadFromFile("images/playButton.png");
+    x6.loadFromFile("images/quitButton.png");
 
-    Sprite sBackground(x1), sPlat(x2), sChars(x3);
-    sBackground.setTextureRect(IntRect(0, 0, 600, 853));
+    Sprite sBackgroundGame(x1), sPlat(x2), sChars(x3),sBackgroundMainMenu(x4) , sPlayButton(x5), sQuitButton(x6);
 
     point plat[20];
 
@@ -32,22 +36,58 @@ int main()
 
     int x = 100, y = 100, h = 200;
     float dx = 0, dy = 0;
+    enum States { MainMenu , Game , GameOver };
+    short unsigned currentState = MainMenu;
+    sPlayButton.setPosition(82, 236);
+    sQuitButton.setPosition(92, 358);
 
     while (app.isOpen())
     {
         Event e;
+        //sQuitButton.setTexture(x6);
         while (app.pollEvent(e))
         {
             if (e.type == Event::Closed)
                 app.close();
         }
 
-        app.draw(sBackground);
-        app.draw(sChars);
-        for (int i = 0; i < 10; i++)
+        if (Keyboard::isKeyPressed(Keyboard::Enter)) currentState = Game;
+        if (sPlayButton.getGlobalBounds().contains(app.mapPixelToCoords(Mouse::getPosition(app))))
         {
-            sPlat.setPosition(plat[i].x, plat[i].y);
-            app.draw(sPlat);
+            if (Mouse::isButtonPressed(Mouse::Left))
+            {
+                currentState = Game;
+            }
+        }
+        if (sQuitButton.getGlobalBounds().contains(app.mapPixelToCoords(Mouse::getPosition(app))))
+        {
+            //sQuitButton.setTexture();
+            if (Mouse::isButtonPressed(Mouse::Left))
+            {
+                app.close();
+            }
+        }
+
+        switch (currentState)
+        {
+        case MainMenu:
+            app.draw(sBackgroundMainMenu);
+            app.draw(sPlayButton);
+            app.draw(sQuitButton);
+            break;
+        case Game:
+            app.draw(sBackgroundGame);
+            for (int i = 0; i < 10; i++)
+            {
+                sPlat.setPosition(plat[i].x, plat[i].y);
+                app.draw(sPlat);
+            }
+            app.draw(sChars);
+            break;
+        case GameOver:
+            break;
+        default:
+            break;
         }
 
         app.display();
