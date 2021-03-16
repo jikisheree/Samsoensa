e8c7b715bcaa9fbd910a93a3e7e6523ff9db261a
NuChyyyy <76035494+NuChyyyy@users.noreply.github.com>
2021-03-17 02:15:39 +0700

    - edit randomize platfrom - edit sound effect - edit freezes / lag when playing (due to the problem of playing music) -create pause button

--- Project1/Project1/Source.cpp
+++ Project1/Project1/Source.cpp
@@ -8,11 +8,11 @@
 using namespace sf;
 using namespace std;
 
-bool checkPlat(Sprite& sPlat,list<Sprite> plats) // Check the platform if it's close to other platforms.
+bool checkPlat(Sprite& sPlat, list<Sprite> plats) // Check the platform if it's close to other platforms.
 {
 	for (auto i = plats.begin(); i != plats.end(); i++)
 	{
-		if (pow((sPlat.getPosition().x - i->getPosition().x),2) + pow((sPlat.getPosition().y - i->getPosition().y),2) < 20000) {
+		if (pow((sPlat.getPosition().x - i->getPosition().x), 2) + pow((sPlat.getPosition().y - i->getPosition().y), 2) < 19000) {
 			return false;
 		}
 	}
@@ -35,26 +35,33 @@ int main()
 	short unsigned currentState = MainMenu;
 
 	//open music from file
-	Music musicmenu, musicgame, musicgameover, musicfish, musicdog, musictouch, musicclick, musicjump;
+	Music musicmenu, musicgame, musicgameover;
+	SoundBuffer bufferFish, bufferDog, bufferClick, bufferJump;
+	bufferFish.loadFromFile("sounds/fish.wav");
+	bufferDog.loadFromFile("sounds/dog.wav");
+	bufferClick.loadFromFile("sounds/click.wav");
+	bufferJump.loadFromFile("sounds/jump.wav");
+	Sound musicfish, musicdog, musicclick, musicjump;
 	musicmenu.openFromFile("sounds/bgm.wav");
 	musicgame.openFromFile("sounds/butter_building.wav");
 	musicgameover.openFromFile("sounds/gameover.wav");
-	musicfish.openFromFile("sounds/fish.wav");
-	musicdog.openFromFile("sounds/dog.wav");
-	musictouch.openFromFile("sounds/touch.wav");
-	musicclick.openFromFile("sounds/click.wav");
-	musicjump.openFromFile("sounds/jump.wav");
+	musicfish.setBuffer(bufferFish);
+	musicdog.setBuffer(bufferDog);
+	musicclick.setBuffer(bufferClick);
+	musicjump.setBuffer(bufferJump);
 	//set loop/volume.
 	musicmenu.play();
-	musicmenu.setVolume(45);
-	musicgame.setVolume(15);
+	musicmenu.setVolume(35);
+	musicgame.setVolume(7);
 	musicgameover.setVolume(12);
-	musicfish.setVolume(12);
-	musicjump.setVolume(10);
+	musicfish.setVolume(9);
+	musicjump.setVolume(20);
+	musicdog.setVolume(50);
+	musicclick.setVolume(800);
 	musicgame.setLoop(true);
 	musicmenu.setLoop(true);
 
-	Texture x1, x2, x3, x4, x5, x6, x7, x8, x9, x10, x11, x12, x13, x14, x15, x16, x17, x18, x19, x20, x21, x22, x23;
+	Texture x1, x2, x3, x4, x5, x6, x7, x8, x9, x10, x11, x12, x13, x14, x15, x16, x17, x18, x19, x20, x21, x22, x23, x24, x25;
 	x1.loadFromFile("images/bg1.jpg");
 	x2.loadFromFile("images/platform.png");
 	x3.loadFromFile("images/cat.png");
@@ -78,10 +85,12 @@ int main()
 	x21.loadFromFile("images/dog.png");
 	x22.loadFromFile("images/fish.png");
 	x23.loadFromFile("images/circle.png");
+	x24.loadFromFile("images/pause.png");
+	x25.loadFromFile("images/pause2.png");
 
 	Sprite sBackgroundGame(x1), sPlat(x2), sChars(x3), sBackgroundMainMenu(x4), sPlayButton(x5), sQuitButton(x6),
 		sBGgameover(x9), sgameover(x10), sNewGameButton(x11), sQuitGOButton(x13), sReplayButton(x17),
-		sResumeButton(x19), sBackgroundPause(x16), sFish(x22), sBlackhole(x23);
+		sResumeButton(x19), sBackgroundPause(x16), sFish(x22), sBlackhole(x23), sPauseButton(x24);
 	Sprite fish[10];
 	Sprite* sDog = NULL;
 	list<Sprite> plats;
@@ -108,10 +117,10 @@ int main()
 	plats.push_back(sPlat);
 	for (auto i = plats.begin(); i != plats.end(); i++)
 	{
-		i->setPosition(rand() % 500, rand() % 1353 - 500);
+		i->setPosition(rand() % 500, rand() % 1253 -400);
 		for (auto j = plats.begin(); j != i; j++)
 		{
-			if (pow((i->getPosition().x - j->getPosition().x),2) + pow((i->getPosition().y - j->getPosition().y),2) < 20000) 
+			if (pow((i->getPosition().x - j->getPosition().x), 2) + pow((i->getPosition().y - j->getPosition().y), 2) < 19000)
 			{
 				i = plats.begin();
 				break;
@@ -127,7 +136,7 @@ int main()
 	//set blackhole
 	if (scores >= 400)
 		sBlackhole.setPosition(rand() % 500, rand() % 780);
-	
+
 	while (app.isOpen())
 	{
 		//time condition
@@ -137,7 +146,7 @@ int main()
 		}
 
 		int time2 = clock2.getElapsedTime().asSeconds();
-				
+
 		Event e;
 		while (app.pollEvent(e))
 		{
@@ -158,7 +167,7 @@ int main()
 			if (Keyboard::isKeyPressed(Keyboard::Enter))
 			{
 				musicclick.stop();
-				musicclick.play(); 
+				musicclick.play();
 				sPlayButton.setTexture(x7);
 				currentState = Game;
 				musicmenu.pause();
@@ -170,7 +179,6 @@ int main()
 				if (Mouse::isButtonPressed(Mouse::Left))
 				{
 					currentState = Game;
-					musicclick.stop();
 					musicclick.play();
 					musicmenu.pause();
 					musicgame.play();
@@ -180,7 +188,7 @@ int main()
 			{
 				sQuitButton.setTexture(x8);
 				if (Mouse::isButtonPressed(Mouse::Left))
-				{					
+				{
 					app.close();
 				}
 			}
@@ -191,26 +199,38 @@ int main()
 			app.draw(sQuitButton);
 			break;
 		case Game:
-
-			//set bLackhole first position
-			if (scores < 400)
-				sBlackhole.setPosition(2000, 2000);
-
 			//set scoretext
 			scoretext.setCharacterSize(50);
 			scoretext.setPosition(15, 3);
 			scoretext.setString("Score : " + to_string(scores));
+			sPauseButton.setTexture(x24);
+			sPauseButton.setPosition(520,10);
 
-			if (Keyboard::isKeyPressed(Keyboard::Right) or Keyboard::isKeyPressed(Keyboard::D)) x += 4.5;
-			if (Keyboard::isKeyPressed(Keyboard::Left) or Keyboard::isKeyPressed(Keyboard::A)) x -= 4.5;
-			if (Keyboard::isKeyPressed(Keyboard::Escape) or Keyboard::isKeyPressed(Keyboard::Space))
+			if (Keyboard::isKeyPressed(Keyboard::Right) || Keyboard::isKeyPressed(Keyboard::D)) x += 4.5;
+			if (Keyboard::isKeyPressed(Keyboard::Left) || Keyboard::isKeyPressed(Keyboard::A)) x -= 4.5;
+			if (Keyboard::isKeyPressed(Keyboard::Escape))
 			{
+				sPauseButton.setTexture(x25);
 				currentState = Pause;
 				musicgame.pause();
 				musicmenu.play();
 			}
+			if (sPauseButton.getGlobalBounds().contains(app.mapPixelToCoords(Mouse::getPosition(app))))
+			{
+				sPauseButton.setTexture(x25);
+				if (Mouse::isButtonPressed(Mouse::Left))
+				{
+					currentState = Pause;
+					musicgame.pause();
+					musicmenu.play();
+				}
+			}
 			dy += 0.49;
 			y += dy;
+
+			//set bLackhole first position
+			if (scores < 400)
+				sBlackhole.setPosition(2000, 2000);
 
 			//if charactor positoin out of window >> change to another side
 			if (x > 600) x = -70;
@@ -222,7 +242,6 @@ int main()
 				if ((x + 65 > i.getPosition().x) && (x + 30 < i.getPosition().x + 112) &&
 					(y + 125 > i.getPosition().y) && (y + 125 < i.getPosition().y + 40) && (dy > 0)) {
 					dy = -20;
-					musicjump.stop();
 					musicjump.play();
 					sChars.setTexture(x15);
 					if (time1 > 1.5) {
@@ -231,120 +250,118 @@ int main()
 				}
 			}
 
-		//Move the platform,fish and dog down and set new position
-		if (y < h)
-		{
-			y = h;
-			for (auto& i : plats)
+			//Move the platform,fish and dog down and set new position
+			if (y < h)
 			{
-				i.setPosition(i.getPosition().x, i.getPosition().y - dy);
-			}
-			for (auto i = plats.begin(); i != plats.end(); i++)
-			{
-				if (i->getPosition().y > 853) {
-					plats.erase(i);
-					break;
-				}
-			}
-			for (int i = 0; i < 10; i++)
-			{
-				fish[i].setPosition(fish[i].getPosition().x, fish[i].getPosition().y - dy);
-				if (fish[i].getPosition().y > 853) { fish[i].setPosition(rand() % 560, -sFish.getGlobalBounds().height); }
-			}
-			if (sDog && sDog->getPosition().y > 853) {
-				delete sDog;
-				sDog = NULL;
-			}
-			if (sDog) sDog->setPosition(sDog->getPosition().x, sDog->getPosition().y - dy);
-			if (plats.size() < 10)
-			{
-				sPlat.setPosition(rand() % 500, -500);
-				if (checkPlat(sPlat, plats))
+				y = h;
+				for (auto& i : plats)
 				{
-					plats.push_back(sPlat);
-					if (!sDog && scores >= 200 && rand () % 5 == 0)
+					i.setPosition(i.getPosition().x, i.getPosition().y - dy);
+				}
+				for (auto i = plats.begin(); i != plats.end(); i++)
+				{
+					if (i->getPosition().y > 853) {
+						plats.erase(i);
+						break;
+					}
+				}
+				for (int i = 0; i < 10; i++)
+				{
+					fish[i].setPosition(fish[i].getPosition().x, fish[i].getPosition().y - dy);
+					if (fish[i].getPosition().y > 853) { fish[i].setPosition(rand() % 560, -sFish.getGlobalBounds().height); }
+				}
+				if (sDog && sDog->getPosition().y > 853) {
+					delete sDog;
+					sDog = NULL;
+				}
+				if (sDog) sDog->setPosition(sDog->getPosition().x, sDog->getPosition().y - dy);
+				if (plats.size() < 10)
+				{
+					sPlat.setPosition(rand() % 500, -400);
+					if (checkPlat(sPlat, plats))
 					{
-						sDog = new Sprite(x21);
-						sDog->setPosition(plats.back().getPosition().x + 10, plats.back().getPosition().y - 115);
+						plats.push_back(sPlat);
+						if (!sDog && scores >= 200 && rand() % 5 == 0)
+						{
+							sDog = new Sprite(x21);
+							sDog->setPosition(plats.back().getPosition().x + 10, plats.back().getPosition().y - 115);
+						}
+					}
+				}
+				if (scores >= 400 && time2 > 15) {
+					sBlackhole.setPosition(sBlackhole.getPosition().x, sBlackhole.getPosition().y - dy);
+					if (sBlackhole.getPosition().y > 853) { sBlackhole.setPosition(rand() % 500, -sBlackhole.getGlobalBounds().height); }
+					if (time2 > 20) {
+						clock2.restart();
+						sBlackhole.setPosition(1000, 1000);
 					}
 				}
 			}
-			if (scores >= 400 && time2 > 15) {
-				sBlackhole.setPosition(sBlackhole.getPosition().x, sBlackhole.getPosition().y - dy);
-				if (sBlackhole.getPosition().y > 853) { sBlackhole.setPosition(rand() % 500, -sBlackhole.getGlobalBounds().height); }
-				if (time2 > 20) {
-					clock2.restart();
-					sBlackhole.setPosition(1000, 1000);
+
+			//if cat Colliding with fish or dog then it dissappear
+			for (int i = 0; i < 10; i++)
+				if (sChars.getGlobalBounds().intersects(fish[i].getGlobalBounds())) {
+					scores += 5;
+					fish[i].setPosition(rand() % 560, -sFish.getGlobalBounds().height); //set new position (dissappear)
+						musicfish.play();
+				}
+			if (sDog && sChars.getGlobalBounds().intersects(sDog->getGlobalBounds())) {
+				scores -= 100;
+				musicdog.play();
+				delete sDog;
+				sDog = NULL;
+			}
+			// meet blackhole >> gameover
+			if (scores >= 400) {
+				if (sChars.getGlobalBounds().intersects(sBlackhole.getGlobalBounds())) {
+					musicgame.stop();
+					musicgameover.play();
+					currentState = GameOver;
 				}
 			}
-		}
 
-		//if cat Colliding with fish or dog then it dissappear
-		
-		for (int i = 0; i < 10; i++) 
-			if (sChars.getGlobalBounds().intersects(fish[i].getGlobalBounds())) {
-				scores += 5;				
-				fish[i].setPosition(rand() % 560, -sFish.getGlobalBounds().height); //set new position (dissappear)
-				musicfish.stop();
-				musicfish.play();
-		}
-		if (sDog && sChars.getGlobalBounds().intersects(sDog->getGlobalBounds())) {
-			scores -= 20;
-			musicdog.stop();
-			musicdog.play();
-			delete sDog;
-			sDog = NULL;
-		}
-		// meet blackhole >> gameover
-		if (scores >= 200) {
-			if (sChars.getGlobalBounds().intersects(sBlackhole.getGlobalBounds())) {
+			//GameOver
+			if (y > 853)
+			{
 				currentState = GameOver;
+				musicgame.pause();
+				musicgameover.play();
 			}
-		}
+			if (scores < 0)
+			{
+				scores = 0;
+				currentState = GameOver;
+				musicgame.pause();
+				musicgameover.play();
+			}
 
-		//GameOver
-		if (y > 853)
-		{
-			currentState = GameOver;
-			musicgame.pause();
-			musicgameover.play();
-		}
-		if (scores < 0)
-		{
-			scores = 0;
-			currentState = GameOver;
-			musicgame.pause();
-			musicgameover.play();
-		}
+			//update cat position
+			sChars.setPosition(x, y);
 
-		//update cat position
-		sChars.setPosition(x, y);
-
-		//render
-		app.draw(sBackgroundGame);
-		for (auto& i : plats)
-		{
-			app.draw(i);
-		}
-		for (int i = 0; i < 10; i++)
-		{
-			app.draw(fish[i]);
-		}
-		if (sDog)
-		{
-			app.draw(*sDog);
-		}
-		if (scores >= 400)
-		{
-			app.draw(sBlackhole);
-		}
-		app.draw(sChars);
-		app.draw(scoretext);
-		break;
+			//render
+			app.draw(sBackgroundGame);
+			for (auto& i : plats)
+			{
+				app.draw(i);
+			}
+			for (int i = 0; i < 10; i++)
+			{
+				app.draw(fish[i]);
+			}
+			if (sDog)
+			{
+				app.draw(*sDog);
+			}
+			if (scores >= 400)
+			{
+				app.draw(sBlackhole);
+			}
+			app.draw(sChars);
+			app.draw(scoretext);
+			app.draw(sPauseButton);
+			break;
 		case GameOver:
-
 			if (scores > highscore) highscore = scores; //Check the score at the end of the game.
-
 			//set everything
 			sgameover.setPosition(22, 78);
 			sNewGameButton.setPosition(155, 549);
@@ -365,10 +382,9 @@ int main()
 				if (Mouse::isButtonPressed(Mouse::Left) or Keyboard::isKeyPressed(Keyboard::Enter) or Keyboard::isKeyPressed(Keyboard::Space))
 				{
 					currentState = Game;
-					musicclick.stop();
 					musicclick.play();
 					musicgame.stop();
-					musicgame.play();					
+					musicgame.play();
 					musicgameover.stop();
 					//newgame set cat&plat position
 					x = 100;
@@ -376,10 +392,10 @@ int main()
 					dy = 0;
 					for (auto i = plats.begin(); i != plats.end(); i++)
 					{
-						i->setPosition(rand() % 500, rand() % 1353 - 500);
+						i->setPosition(rand() % 500, rand() % 1253 - 400);
 						for (auto j = plats.begin(); j != i; j++)
 						{
-							if (pow((i->getPosition().x - j->getPosition().x), 2) + pow((i->getPosition().y - j->getPosition().y), 2) < 20000) 
+							if (pow((i->getPosition().x - j->getPosition().x), 2) + pow((i->getPosition().y - j->getPosition().y), 2) < 19000)
 							{
 								i = plats.begin();
 								break;
@@ -427,6 +443,7 @@ int main()
 			{
 				sResumeButton.setTexture(x20);
 				currentState = Game;
+				musicclick.play();
 				musicgame.play();
 				musicmenu.pause();
 			}
@@ -436,7 +453,6 @@ int main()
 				if (Mouse::isButtonPressed(Mouse::Left))
 				{
 					currentState = Game;
-					musicclick.stop();
 					musicclick.play();
 					musicgame.play();
 					musicmenu.pause();
@@ -449,8 +465,8 @@ int main()
 				if (Mouse::isButtonPressed(Mouse::Left))
 				{
 					currentState = Game;
-					musicclick.stop();
-					musicclick.play(); 
+					scores = 0;
+					musicclick.play();
 					musicgame.stop();
 					musicgame.play();
 					musicmenu.pause();
@@ -460,10 +476,10 @@ int main()
 					dy = 0;
 					for (auto i = plats.begin(); i != plats.end(); i++)
 					{
-						i->setPosition(rand() % 500, rand() % 1353 - 500);
+						i->setPosition(rand() % 500, rand() % 1253 - 400);
 						for (auto j = plats.begin(); j != i; j++)
 						{
-							if (pow((i->getPosition().x - j->getPosition().x), 2) + pow((i->getPosition().y - j->getPosition().y), 2) < 20000) 
+							if (pow((i->getPosition().x - j->getPosition().x), 2) + pow((i->getPosition().y - j->getPosition().y), 2) < 19000)
 							{
 								i = plats.begin();
 								break;
@@ -487,7 +503,7 @@ int main()
 					app.close();
 				}
 			}
-			
+
 			//Render
 			app.draw(sBackgroundPause);
 			app.draw(sResumeButton);
@@ -496,8 +512,8 @@ int main()
 			break;
 		default:
 			break;
+		}
+		app.display();
 	}
-	app.display();
-}
-return 0;
+	return 0;
 }
